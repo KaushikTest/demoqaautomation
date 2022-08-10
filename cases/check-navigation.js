1665f972e2ad760b733802973b77aeb8927ef4af
KaushikTest <kaushik6990@gmail.com>
2022-08-10 19:01:11 +0530

    Refactor
    Running in multiple browsers

--- cases/check-navigation.js
+++ cases/check-navigation.js
@@ -2,45 +2,48 @@
 import { ALERTS, BOOKSTORE, ELEMENTS, FORMS, INTERACTIONS, WIDGETS } from '../constants/demoqapageconstants';
 import { BASELINK } from '../constants/linkconstants';
 import { DemoWelcomePageClickElements } from '../pages/demoqawelcomepage';
-import { getPage } from '../pages/common';
 
-export async function testElementsNavigation(){
-    const page = await getPage();
+
+export async function testElementsNavigation(page){
     await page.goto(BASELINK);
     await DemoWelcomePageClickElements(page,ELEMENTS);
     return page;
 }
  
-export async function testFormsNavigation(){
-    const page = await getPage();
+
+export async function testFormsNavigation(page){
     await page.goto(BASELINK);
     await DemoWelcomePageClickElements(page,FORMS);
     return page;
 }
  
-export async function testAlertsNavigation(){
-    const page = await getPage();
+
+export async function testAlertsNavigation(page){
+
     await page.goto(BASELINK);
     await DemoWelcomePageClickElements(page,ALERTS);
     return page;
 }
  
-export async function testWidgetsNavigation(){
-    const page = await getPage();
+
+export async function testWidgetsNavigation(page){
+
     await page.goto(BASELINK);
     await DemoWelcomePageClickElements(page,WIDGETS);
     return page;
 }
  
-export async function testInteractionsNavigation(){
-    const page = await getPage();
+
+export async function testInteractionsNavigation(page){
+
     await page.goto(BASELINK);
     await DemoWelcomePageClickElements(page,INTERACTIONS);
     return page;
 }
 
-export async function testBookStoreNavigation(){
-    const page = await getPage();
+
+export async function testBookStoreNavigation(page){
+
     await page.goto(BASELINK);
     await DemoWelcomePageClickElements(page,BOOKSTORE);
     return page;
--- tests/check-navigation.spec.js
+++ tests/check-navigation.spec.js
@@ -1,45 +1,46 @@
 // @ts-check
 import { expect, test } from '@playwright/test';
 import * as DemoCases from '../cases/check-navigation';
-import { ALERTSLINK, BOOKSTORELINK, ELEMENTSLINK, FORMSLINK, INTERACTIONSLINK, WIDGETSLINK } from '../constants/linkconstants.js';
+import { ALERTSLINK, BASELINK, BOOKSTORELINK, ELEMENTSLINK, FORMSLINK, INTERACTIONSLINK, WIDGETSLINK } from '../constants/linkconstants.js';
 
 test.use({
     viewport: { width: 1920, height: 1080 },
+    headless: false
 });
 
-test('testNavigationToElements', async () => {
-    const page = await DemoCases.testElementsNavigation();
+test('testNavigationToElements', async ({page}) => {
+    await DemoCases.testElementsNavigation(page);
     expect(page.url()).toEqual(ELEMENTSLINK);
     await page.close();
 });
 
-test('testFormsNavigation', async () => {
-    const page = await DemoCases.testFormsNavigation();
+test('testFormsNavigation', async ({page}) => {
+    await DemoCases.testFormsNavigation(page);
     expect(page.url()).toEqual(FORMSLINK);
     await page.close();
 });
 
-test('testAlertsNavigation', async () => {
-    const page = await DemoCases.testAlertsNavigation();
+test('testAlertsNavigation', async ({page}) => {
+    await DemoCases.testAlertsNavigation(page);
     expect(page.url()).toEqual(ALERTSLINK);
     await page.close();
 });
 
 
-test('testWidgetsNavigation', async () => {
-    const page = await DemoCases.testWidgetsNavigation();
+test('testWidgetsNavigation', async ({page}) => {
+    await DemoCases.testWidgetsNavigation(page);
     expect(page.url()).toEqual(WIDGETSLINK);
     await page.close();
 });
 
-test('testInteractionsNavigation', async () => {
-    const page = await DemoCases.testInteractionsNavigation();
+test('testInteractionsNavigation', async ({page}) => {
+    await DemoCases.testInteractionsNavigation(page);
     expect(page.url()).toEqual(INTERACTIONSLINK);
     await page.close();
 });
 
-test('testBookStoreNavigation', async () => {
-    const page = await DemoCases.testBookStoreNavigation();
+test('testBookStoreNavigation', async ({page}) => {
+    await DemoCases.testBookStoreNavigation(page);
     expect(page.url()).toEqual(BOOKSTORELINK);
     await page.close();
 });
